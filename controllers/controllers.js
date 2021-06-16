4c34c749e66f47dee80a68863d7f8b34810639c0
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2021-06-16 22:16:56 +0200

    email format guessing functionality

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,15 +1,63 @@
-const ahoyThere = (req, res, next) => {
-    res.status(200).json({
-        body: 'Ahoy there from the server!'
-    });
+const sampleData = require ("../sampleData.json");
+
+
+function ahoyThere(req, res, next){
+
+    if (validateDomain(req.body.Domain)){
+        let responseData = validateDomain(req.body.Domain);
+
+        res.status(200).json({
+            body: responseData
+        });
+    } else {
+        let responseData = "Sorry we can't guess the email format for that domain :("
+
+        res.status(200).json({
+            body: responseData
+        });
+    }
 };
+
 
 module.exports.ahoyThere = ahoyThere;
 
-const getEmail = (req, res, next) => {
-    res.status(200).json({
-        body: 'email'
-    });
-};
+function guessEmail(inputData) {
+    return 'wheyy';
+}
 
-module.exports.getEmail = getEmail;
+function identifyEmailFormat(emailAddress, fullName) {
+    let emailString = emailAddress.substring(0, emailAddress.indexOf('@'));
+    let fullNameString = fullName.replace(' ', '').toLowerCase();
+    
+    if (emailString === fullNameString) {
+        return 'fullname email format';
+    }
+    return 'initial email format';
+}
+
+
+function validateDomain(domain) {
+    let sampleEmail = null
+
+    for(let i = 0; i < sampleData.length; i++){
+        if(sampleData[i].Email.includes(domain)){
+            sampleEmail = identifyEmailFormat(sampleData[i].Email, sampleData[i].FullName);
+            break
+        }
+    }
+    return sampleEmail;
+}
+
+
+function emailFormat(email) {
+    return email
+
+}
+
+// check if domain is known in sample data
+
+// check if format is full name or just initial
+
+// create email address for input data
+
+// return email address or 'sorry'
--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const controllers = require('./../controllers/controllers');
 
 router.post('/ahoy-there', controllers.ahoyThere);
-router.get('/email-address', controllers.getEmail);
 
 module.exports = router;
